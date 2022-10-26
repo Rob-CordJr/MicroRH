5a7ea0a50e922a97d0548d8cb7a50f774e3d5494
Roberto Junior <robertodossantos747@gmail.com>
2022-10-26 13:40:54 -0300

    Animação Sidenav Concluida

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,13 +1,16 @@
+
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { map, Observable, shareReplay } from 'rxjs';
 import { User } from '../models/User';
+import { sideNavAnimation, sideNavContainerAnimation } from './sidebar-animations';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css']
+  styleUrls: ['./main-nav.component.css'],
+  animations: [sideNavAnimation, sideNavContainerAnimation]
 })
 
 
@@ -17,7 +20,7 @@ export class MainNavComponent implements OnInit {
   @ViewChild('sidenav') sidenav: MatSidenav | any;
   isExpanded: boolean = false;
   showSubmenu: boolean = false;
-  opened: boolean = true;
+  isOpen: boolean = false;
   showSubSubMenu: boolean = false;
   isImageLoading: boolean = false;
   imageToShow: any;
@@ -40,6 +43,9 @@ export class MainNavComponent implements OnInit {
 
   }
 
+  toggle() {
+     this.isOpen = !this.isOpen;
+   }
  
   // mouseenter() {
   //   if (!this.isExpanded) {
