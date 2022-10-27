9aa0fb203cdff4d436945dc8f43039280aeaa266
Roberto Junior <robertodossantos747@gmail.com>
2022-10-27 12:30:14 -0300

    Modificações de Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { CalendarComponent } from './calendar/calendar.component';
 import { LoginComponent } from './login/login.component';
 import { NotasAdesivasComponent } from './notas-adesivas/notas-adesivas.component';
 import { PainelControleComponent } from './painel-controle/painel-controle.component';
+import { TimesheetComponent } from './timesheet/timesheet.component';
+import { TreinamentoComponent } from './treinamento/treinamento.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'painel', component: PainelControleComponent},
   {path: 'calendar', component: CalendarComponent},
   {path: 'notas', component: NotasAdesivasComponent},
+  {path: 'timesheet', component: TimesheetComponent},
+  {path: 'treinamento', component: TreinamentoComponent},
 ];
 
 @NgModule({
